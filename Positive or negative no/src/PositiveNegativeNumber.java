126a96a114b1610f815579c37fb09d0d62e5ba13
tripathi <mayanktripathi028>
2020-08-18 23:32:53 +0530

    Positive negative number

---
+++ Positive or negative no/src/PositiveNegativeNumber.java
@@ -0,0 +1,19 @@
+public class PositiveNegativeNumber {
+
+    public static void main(String[] args)
+    {
+        int number=26;
+        if(number > 0)
+        {
+            System.out.println(number+" is a positive number");
+        }
+        else if(number < 0)
+        {
+            System.out.println(number+" is a negative number");
+        }
+        else
+        {
+            System.out.println(number+" is neither positive nor negative");
+        }
+    }
+}
