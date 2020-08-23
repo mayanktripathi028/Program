efffb3f9b3cdda04deb81a1d4e9d0f6ed85e71cf
tripathi <mayanktripathi028>
2020-08-24 01:34:01 +0530

    Operator

---
+++ Operators/src/LogicOperator.java
@@ -0,0 +1,9 @@
+public class LogicOperator {
+    public static void main(String[]args){
+        boolean b1= true;
+        boolean b2= false;
+        System.out.println("b1&&b2:" +(b1&&b2));
+        System.out.println("b1||b2:" +(b1||b2));
+        System.out.println("!(b2&&b1):" +!(b1&&b2));
+    }
+}
