b940b70c92aa6c8e2f0368e96364291202d34c44
tripathi <mayanktripathi028>
2020-08-28 17:38:12 +0530

    For Loop

---
+++ for loop/src/ForLoop2.java
@@ -0,0 +1,11 @@
+// sum of natural number
+public class ForLoop2 {
+        public static void main(String[] args) {
+            int sum = 0;
+            for (int i = 3; i <= 6; ++i) {
+                sum += i;     // sum = sum + i
+            }
+            System.out.println("Sum = " + sum);
+        }
+    }
+
