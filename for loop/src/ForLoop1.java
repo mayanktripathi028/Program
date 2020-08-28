f68fea60752179f324554699f349f913524e2047
tripathi <mayanktripathi028>
2020-08-28 16:06:20 +0530

    For Loop

---
+++ for loop/src/ForLoop1.java
@@ -0,0 +1,9 @@
+public class ForLoop1 {
+    public static void main(String[]args){
+        for(int i= 10; i>1; i--){
+            System.out.println("The value of i is:" +i);
+        }
+
+    }
+}
+// if we use 'i++' instead of 'i--' it goes to infinite number
