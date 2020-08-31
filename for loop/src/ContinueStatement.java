5aee549d928bdbcde48c3e1fb79b21ba36ebe3ac
tripathi <mayanktripathi028>
2020-09-01 01:56:02 +0530

    For Loop

---
+++ for loop/src/ContinueStatement.java
@@ -0,0 +1,11 @@
+public class ContinueStatement {
+    public static void main(String[]args){
+        for (int i=0; i<=8; i++)
+        {
+            if (i==3){
+                continue;
+            }
+            System.out.println(i+"");
+        }
+    }
+}
