f7dc2d1bdd1549f750e9718dd7e5db308f595827
tripathi <mayanktripathi028>
2020-09-21 21:26:39 +0530

    Array

---
+++ Array/src/AnonymousArray.java
@@ -0,0 +1,12 @@
+public class AnonymousArray {
+    public static void main(String[]args){
+        AnonymousArray.sum(new int[]{10,20,30});
+    }
+    static void sum(int[] no){
+        int total=0;
+        for (int i:no){
+            total=total+i;
+        }
+        System.out.println("Sum of Numbers:"+total);
+    }
+}
